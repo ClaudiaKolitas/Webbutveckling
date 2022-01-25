8bb59bfa0557f7e84df662865cb906c5bd08c83e
ClaudiaKolitas <Claudia.kolitas@chasacademy.se>
2022-01-25 11:45:48 +0100

    24 januari javascript lektion

---
+++ Tisdag 24 januari/script.js
@@ -0,0 +1,85 @@
+//addition
+/*
+let x = 30;
+let b = 10;
+let sum = x + b;
+document.write("Summan är: " ,sum)
+*/
+
+// subtraktion
+/*
+let x = 30;
+let b = 10;
+let sum = x - b;
+document.write("Summa är: ",sum)
+*/
+
+// multiplikation
+/*
+let x = 30;
+let b = 10;
+let sum = x * b;
+document.write("Summan är: ",sum);
+*/
+
+// exponential tal, upphöjt till
+/*
+let x = 5;
+let b = 2;
+let sum = x ** b;
+document.write("Summan är: ",sum);
+*/
+
+// division
+/*
+let x = 30;
+let b = 10;
+let sum = x/b;
+document.write("Summan är: ",sum);
+*/
+
+//modolus
+/*
+let x = 6;
+let b = 5;
+let sum = x%b;
+document.write("Summan är: ",sum);
+*/
+
+// ökning increment
+/*
+let x = 5;
+x++
+let sum = x;
+document.write("Summan är: ",sum);
+*/
+
+// Sänkning decrementera
+/*
+let x = 5;
+x--
+let sum = x;
+document.write("Summan är: ",sum);
+*/
+
+// getElementById
+/*
+ let a = 10;
+ let b = 20;
+ let sum = a + b;
+ document.getElementById("test").innerHTML=" The value of sum is: " + sum;
+ document.getElementById("test2").innerHTML=" The value of sum is: " + sum;
+ */
+
+ // alert i webbläsaren
+ /*
+ window.alert("hello snygging");
+*/
+
+// console.log()
+/*
+let a = 5;
+let b = 15;
+console.log("The value of a + b is", a+b); */
+
+
